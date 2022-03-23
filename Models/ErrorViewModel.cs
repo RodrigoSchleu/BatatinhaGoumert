ceb32a80e1c7991b502f4f6fc1864615c98995ba
Rodrigo <schleurodrigo@gmail.com>
2022-03-22 21:23:46 -0300

    Feat: create project structure

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BatatinhaGourmert.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
